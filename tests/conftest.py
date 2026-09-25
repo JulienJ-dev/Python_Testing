bad890f13cb1153ef720be9ac209f2f62fe1bab6
JulienJ-dev <julienjedrzejewski@gmail.com>
2026-09-25 11:21:19 +0200

    Isolate application data between tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,18 @@
+import copy
+
 import pytest
 
 import server
+
+
+INITIAL_CLUBS = copy.deepcopy(server.clubs)
+INITIAL_COMPETITIONS = copy.deepcopy(server.competitions)
+
+
+@pytest.fixture(autouse=True)
+def reset_data(monkeypatch):
+    monkeypatch.setattr(server, "clubs", copy.deepcopy(INITIAL_CLUBS))
+    monkeypatch.setattr(server, "competitions", copy.deepcopy(INITIAL_COMPETITIONS))
 
 
 @pytest.fixture
